d881b8e04b28d64ac1c78502dd367ce64c82a378
DragonTequila <diooplatinum@gmail.com>
2023-06-13 10:12:22 +0300

    First commit. Added a password system that is very weak but who cares because only admin has a separate password.

---
+++ backgroundProcess.php
@@ -0,0 +1,11 @@
+<?php
+
+// Do something with the username and password, e.g., store them in a database
+
+// Return a response if needed
+echo "Data received successfully!";
+
+
+$password = Encipher('anna22', 3);
+
+?>
